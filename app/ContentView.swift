7860518081a9d3ed571aff968207536fa4e25883
Andrew Tan <andrewvmail@gmail.com>
2020-04-16 00:15:47 -0700

    init

---
+++ app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Andrew Tan on 2020-04-15.
+//  Copyright © 2020 Andrew Tan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
